9a88b6e5b6f3578986e7ccf8d303c3bf6af98b36
UsielOM <usiel31197@outlook.es>
2022-09-14 12:40:25 -0500

    Utilizando variables en mi template

--- src/hola-mundo.js
+++ src/hola-mundo.js
@@ -1,11 +1,17 @@
 import { LitElement, html } from "lit-element"; //Este es el import de la libreria lit element
 
 class HolaMundo extends LitElement {
+    static get properties() {
+        return {
+            quien: { type: String }
+        };
+    }
+
     render() { //Me permite definir la parte vizual del componente 
         return html `
-        <p>Hola mundo </p>
+        <p>Hola  <b>${this.quien}</b></p>
         `;
-        //TEMPLATE STRING 
+        //TEMPLATE STRINGS 
     }
 }
 customElements.define('hola-mundo', HolaMundo); //Define un nuevo componente
